e5a481d0e660a8df4298ed806f9d5b807da698d2
Zach <zbunde@gmail.com>
2015-09-21 18:00:17 -0600

    step 3

--- snake.js
+++ snake.js
@@ -1,14 +1,25 @@
 
 /// only call our draw function when the canvas is loaded and has context!
 document.addEventListener("DOMContentLoaded", function() {
-  drawBorder();
-});
-//
+  Game()
+  });
 
 //// Step 1: Drawing a Border
-function drawBorder(){
+function drawBorder(color){
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext('2d');
-    context.strokeStyle = '#E28D05'
+    context.strokeStyle = color
+    context.lineWidth=10;
     context.strokeRect(0,0, canvas.width, canvas.height);
+}
+
+/// Step 3: Intervals
+function Game (){
+      var interval = function() {
+          drawBorder('orange')
+          setTimeout(function() {
+            requestAnimationFrame(interval);
+          }, 1000);
+    };
+    interval();
 }
