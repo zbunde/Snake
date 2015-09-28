83350d2a922896b2549fa6a340d85c682f5bb90f
Zach <zbunde@gmail.com>
2015-09-28 13:55:05 -0600

    update interval branch

--- snake.js
+++ snake.js
@@ -1,14 +1,32 @@
 
 /// only call our draw function when the canvas is loaded and has context!
 document.addEventListener("DOMContentLoaded", function() {
-  drawBorder();
+    Game();
 });
 //
 
 //// Step 1: Drawing a Border
-function drawBorder(){
+function drawBorder(color){
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext('2d');
-    context.strokeStyle = '#E28D05'
+    context.strokeStyle = color
+    context.lineWidth=10;
     context.strokeRect(0,0, canvas.width, canvas.height);
+}
+function Game (){
+      var count = 0
+      var interval = function() {
+        count ++
+        if(count % 2 == 0){
+          drawBorder('green');
+        }
+        else {
+          drawBorder('red')
+        }
+
+          setTimeout(function() {
+            requestAnimationFrame(interval);
+          }, 1000);
+    };
+    interval();
 }
