ce8cd66a37cccf5c3fc6e4bbc7d45878b04d52c2
Zach <zbunde@gmail.com>
2015-09-28 17:50:12 -0600

    fix step11

--- snake.js
+++ snake.js
@@ -28,12 +28,11 @@ var  Game = function (){
           drawBorder('orange', self.score);
           updateSnake(self.snake);
           updateSnake(self.computer);
-          updateComputer(self.computer, self.food)
+          updateComputer(self.snake, self.food, self.computer)
           drawSnake(self.snake);
           drawSnake(self.computer);
           drawFood(self.food);
           checkCollision(self.snake, self.computer, self.food)
-
           setTimeout(function() {
             requestAnimationFrame(interval);
         }, self.speed);
@@ -64,7 +63,6 @@ var Snake = function(y, color){
       for(var i = self.length; i>=0; i--) {
         self.snakeArray.push({x: i, y:y});
       }
-
 }
 
 /// Step 5 Movement
@@ -119,32 +117,42 @@ function drawFood(food){
 
 function checkCollision(snake, computer, food){
   var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d')
+
   var pixelSize = canvas.width / 25
         /// If we run into the computer.
+
         for(i=0; i< computer.snakeArray.length; i++){
-            if(snake.snakeArray[0].x === computer.snakeArray[i].x && snake.snakeArray[0].y === computer.snakeArray[i].y)
-                drawWinner("COMPUTER");
+            if(snake.snakeArray[0].x === computer.snakeArray[i].x && snake.snakeArray[0].y === computer.snakeArray[i].y){
+                //  alert("COMPUTER");
+                 alert("You lose!")
+                 location.reload()
+                console.log('hi')
             }
-
-            /// If the computer runs into us
-        for(i=0; i< snake.snakeArray.length; i++){
-            if(computer.snakeArray[0].x === snake.snakeArray[i].x && computer.snakeArray[0].y === snake.snakeArray[i].y)
-            drawWinner("You win!");
-            }
-
-    ///  If the computer hits the food.
-    if(computer.snakeArray[0].x === food.x && computer.snakeArray[0].y === food.y){
-        game.food = createFood()
-        game.score--
-        if(game.speed > 30){
-            game.speed = game.speed - 15
         }
-        var tail = {}
-        tail.x = computer.snakeArray[0].x
-        tail.y = computer.snakeArray[0].y
-        computer.snakeArray.unshift(tail);
-      }
-    /// if we hit our food!
+
+        /// If the computer runs into us
+        for(i=0; i< snake.snakeArray.length; i++){
+            if(computer.snakeArray[0].x === snake.snakeArray[i].x && computer.snakeArray[0].y === snake.snakeArray[i].y){
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                  alert("You win!");
+                  location.reload()
+            }
+        }
+
+        ///  If the computer hits the food.
+        if(computer.snakeArray[0].x === food.x && computer.snakeArray[0].y === food.y){
+            game.food = createFood()
+            game.score--
+            if(game.speed > 30){
+                game.speed = game.speed - 15
+            }
+            var tail = {}
+            tail.x = computer.snakeArray[0].x
+            tail.y = computer.snakeArray[0].y
+            computer.snakeArray.unshift(tail);
+          }
+        /// if we hit our food!
         if(snake.snakeArray[0].x === food.x && snake.snakeArray[0].y === food.y){
             game.food = createFood()
             game.score++
@@ -157,98 +165,130 @@ function checkCollision(snake, computer, food){
             snake.snakeArray.unshift(tail);
           }
         /// off the map left
-    if(snake.snakeArray[0].x < 0 ){
-      location.reload();
-    }
-    /// off the map up
-    if(snake.snakeArray[0].y < 0 ){
-      location.reload();
+        if(snake.snakeArray[0].x < 0 ){
+            alert("You lose!");
+            location.reload();
+        }
+        /// Computer off the map left
+        if(computer.snakeArray[0].x < 0 ){
+            alert("You win!");
+            location.reload();
+        }
+        /// off the map up
+        if(snake.snakeArray[0].y < 0 ){
+            alert("You lose!");
+            location.reload();
+        }
+        /// Computer off the map up
+        if(computer.snakeArray[0].y < 0 ){
+            alert("You win!");
+            location.reload();
+        }
+        /// off the map down
+        if(snake.snakeArray[0].y + 1 > 600/pixelSize ){
+            alert("Computer wins!");
+            location.reload();
+        }
+        /// Computer off the map down
+        if(computer.snakeArray[0].y + 1 > 600/pixelSize ){
+            alert("You win!");
+            location.reload();
+        }
 
-    }
-    /// off the map down
-    if(snake.snakeArray[0].y + 1 > 600/pixelSize ){
-      location.reload();
-    }
-    /// off the map right
-    if(snake.snakeArray[0].x + 1 > 600/pixelSize ){
-      location.reload();
-
-    }
-    /// if we hit our own snake body
-    for(var i = 2; i < snake.snakeArray.length; i++){
-      if(snake.snakeArray[i].x == snake.snakeArray[0].x && snake.snakeArray[i].y == snake.snakeArray[0].y){
-        location.reload();
-      }
-
-    }
+        /// off the map right
+        if(snake.snakeArray[0].x + 1 > 600/pixelSize ){
+            alert("You lose!");
+            location.reload();
+        }
+        /// Computer off the map right
+        if(computer.snakeArray[0].x + 1 > 600/pixelSize ){
+            alert("You win!");
+            location.reload();
+        }
+        /// if we hit our own snake body
+        for(var i = 2; i < snake.snakeArray.length; i++){
+          if(snake.snakeArray[i].x == snake.snakeArray[0].x && snake.snakeArray[i].y == snake.snakeArray[0].y){
+              alert("You lose!");
+              location.reload();
+          }
+        }
+          /// if computer hits it's own body
+          for(var i = 2; i < computer.snakeArray.length; i++){
+            if(computer.snakeArray[i].x == computer.snakeArray[0].x && computer.snakeArray[i].y == computer.snakeArray[0].y){
+                alert("You win!");
+                location.reload();
+            }
+        }
 
 }
 
 /// Step 11 Update Computer position.
-function updateComputer(snake, food){
-    if (food.x === snake.snakeArray[0].x) {
-        if (food.y > snake.snakeArray[0].y) {
-          if (isSafe(snake, 'down')) snake.direction = 'down';
+function updateComputer(snake, food, computer){
+    if (food.x === computer.snakeArray[0].x) {
+        if (food.y > computer.snakeArray[0].y) {
+          if (isSafe(snake, computer, 'down')) computer.direction = 'down';
             } else {
-          if (isSafe(snake, 'up')) snake.direction = 'up';
+          if (isSafe(snake, computer, 'up')) computer.direction = 'up';
         }
       }
-      if (food.y === snake.snakeArray[0].y) {
-        if (food.x > snake.snakeArray[0].x) {
-          if (isSafe(snake, 'right')) snake.direction = 'right';
+      if (food.y === computer.snakeArray[0].y) {
+        if (food.x > computer.snakeArray[0].x) {
+          if (isSafe(snake, computer, 'right')) computer.direction = 'right';
         } else {
-          if (isSafe(snake, 'left')) snake.direction = 'left';
+          if (isSafe(snake, computer, 'left')) computer.direction = 'left';
         }
       }
-      if (food.y > snake.snakeArray[0].y) {
-        if (isSafe(snake, 'down')) snake.direction = 'down';
-    } else if (food.y < snake.snakeArray[0].y) {
-        if (isSafe(snake, 'up')) snake.direction = 'up';
+      if (food.y > computer.snakeArray[0].y) {
+        if (isSafe(snake, computer, 'down')) computer.direction = 'down';
+    } else if (food.y < computer.snakeArray[0].y) {
+        if (isSafe(snake, computer, 'up')) computer.direction = 'up';
       }
-      if (food.x > snake.snakeArray[0].x) {
-        if (isSafe(snake, 'right')) snake.direction = 'right';
-    } else if (food.x < snake.snakeArray[0].x) {
-        if (isSafe(snake, 'left')) snake.direction = 'left';
+      if (food.x > computer.snakeArray[0].x) {
+        if (isSafe(snake, computer, 'right')) computer.direction = 'right';
+    } else if (food.x < computer.snakeArray[0].x) {
+        if (isSafe(snake, computer, 'left')) computer.direction = 'left';
       }
-};
+}
 
-function isSafe(snake, dir){
-    var snakeCopy = deepCopy(snake.snakeArray);
+function isSafe(snake, computer, dir){
+  var snakeCopy = deepCopy(snake.snakeArray);
+  var compCopy = deepCopy(computer.snakeArray);
    switch (dir) {
      case 'left':
        for (var i = 0; i < snakeCopy.length/4; i++) {
-         snakeCopy[0].x--;
-         if (checkBodyCollision(snakeCopy[0], snakeCopy)) {
+         compCopy[0].x--;
+         if ( checkBodyCollision(compCopy[0], compCopy) || checkBodyCollision(compCopy[0], snakeCopy)) {
            return false;
          }
        }
        return true;
      case 'right':
-       for (var i = 0; i < snakeCopy.length/4; i++) {
-         snakeCopy[0].x++;
-         if (checkBodyCollision(snakeCopy[0], snakeCopy)) {
+       for (var i = 0; i < compCopy.length/4; i++) {
+         compCopy[0].x++;
+         if (checkBodyCollision(compCopy[0], compCopy) || checkBodyCollision(compCopy[0], snakeCopy)) {
            return false;
          }
        }
        return true;
      case 'up':
-       for (var i = 0; i < snakeCopy.length/4; i++) {
-         snakeCopy[0].y--;
-         if (checkBodyCollision(snakeCopy[0], snakeCopy)) {
+       for (var i = 0; i < compCopy.length/4; i++) {
+         compCopy[0].y--;
+         if (checkBodyCollision(compCopy[0], compCopy) || checkBodyCollision(compCopy[0], snakeCopy)) {
            return false;
          }
        }
        return true;
      case 'down':
-       for (var i = 0; i < snakeCopy.length/4; i++) {
-         snakeCopy[0].y++;
-         if (checkBodyCollision(snakeCopy[0], snakeCopy)) {
+       for (var i = 0; i < compCopy.length/4; i++) {
+         compCopy[0].y++;
+         if (checkBodyCollision(compCopy[0], compCopy) || checkBodyCollision(compCopy[0], snakeCopy)) {
            return false;
          }
        }
        return true;
    }
 }
+
 
 function checkBodyCollision(head, array) {
   for(var i = 1; i < array.length; i++){
@@ -270,12 +310,4 @@ function deepCopy (arr) {
         out.push(obj);
     }
     return out;
-}
-
-function drawWinner(text){
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.font = "30px Arial";
-        context.fillText(text, 30, 30)
 }
